3ad4b44f2863f6aa2c7990587cf94bb5df9e7a8f
Vlastiss <120052173+Vlastiss@users.noreply.github.com>
2024-01-12 17:18:42 +0000

    final version

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-$
+$(document).ready(function () {
+  $("#btn").click(function(){
+   var randomColor = getRandomColor()
+   $("body").css("background-color", randomColor);
+  });
+
+  function getRandomColor() {
+    var letters = "0123456789ABCDEF"
+    var color = "#"
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    $("#color").text(color);
+    return color;
+  }
+});
+
