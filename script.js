4b224fab2b0c5f8e0f08c1498cfc156df9daa077
Vlastiss <120052173+Vlastiss@users.noreply.github.com>
2024-01-12 18:47:23 +0000

    final version

--- script.js
+++ script.js
@@ -1,17 +1,58 @@
 $(document).ready(function () {
-  $("#btn").click(function(){
-   var randomColor = getRandomColor()
-   $("body").css("background-color", randomColor);
+
+  alert("Choose between Simple Color and Hex Color, then click the button ;)");
+
+  $("#simpleColor").click(function () {
+    $("#btn1").removeClass("d-none");
+    $("#simpleColor").addClass("text-primary");
+    $("#hexColor").removeClass("text-primary");
+    if ($("#btn2").hasClass("d-none")) {
+      return;
+    } else {
+      $("#btn2").addClass("d-none");
+    }
   });
 
-  function getRandomColor() {
-    var letters = "0123456789ABCDEF"
-    var color = "#"
-    for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
+  $("#hexColor").click(function () {
+    $("#btn2").removeClass("d-none");
+    $("#hexColor").addClass("text-primary");
+    $("#simpleColor").removeClass("text-primary");
+    if ($("#btn1").hasClass("d-none")) {
+      return;
+    } else {
+      $("#btn1").addClass("d-none");
     }
+  });
+
+  $("#btn1").click(function () {
+    var randomColor = getRandomColor()
+    $("body").css("background-color", randomColor);
+  });
+
+
+  $("#btn2").click(function () {
+    var randomColor = getRandomColor()
+    $("body").css("background-color", getRandomHexColor);
+  });
+
+
+  function getRandomHexColor() {
+    var letters = "0123456789ABCDEF"
+    var hexColor = "#"
+    for (let i = 0; i < 6; i++) {
+      hexColor += letters[Math.floor(Math.random() * 16)];
+    }
+    $("#color").text(hexColor);
+    return hexColor;
+  }
+
+  function getRandomColor() {
+    var colors = ['red', 'green', 'blue', 'white', 'black', 'yellow', 'purple'];
+    var color = colors[Math.floor(Math.random() * 7)]
     $("#color").text(color);
     return color;
   }
+
+
 });
 
